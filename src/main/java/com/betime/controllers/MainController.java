2466256e7ec3925b15768d738bb9dff7cc7732e9
chiwa <kchiwa@gmail.com>
2015-07-16 19:37:45 +0700

    add controller and project structure

---
+++ src/main/java/com/betime/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.betime.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping(value = "/index.html")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+   <h3>Hello World!!!!</h3>
+</body>
+</html>
